da2ec0308dbbd574ee615f98904d57397b73cfc6
8 Bit Pixel <155176453+8-bit-pixel@users.noreply.github.com>
2025-07-31 14:04:33 +0100

    Update curse.cpp

--- curse.cpp
+++ curse.cpp
@@ -1,17 +1,22 @@
 /* This file is licensed under the zlib license (see LICENSE.txt)
  * This file should compile and run on the latest versions of GCC, LLVM Clang and MSVC.
  * For the best user experience, compile with the "-trigraphs" flag on GCC and Clang, or "/Zc:trigraphs" on MSVC
+ * This file has been successfully compiled and run as C++20 code with the following compilers:
+ *  - x86-64 GCC 15.1
+ *  - x86-64 Clang 20.1.0
+ *  - x86 MSVC v19.43 VS17.13
+ *  - x64 MSVC v19.43 VS17.13
  */
 
 #include <iostream>
 #include <iso646.h>
 
-
-template<typename Tea> requires requires(Tea t) { 1 + t; } 
-[[noreturn]] const void * const curse (const Tea and biscuit) {
+template<typename Tea, class ... > requires requires(Tea t) { 1+t+1; } 
+[[noreturn]] inline static const void * const curse (const Tea and biscuit) noexcept {
+    goto https;
 https://www.github.com/8-bit-pixel
-    const char * const $ = "H""e""l""l""o"" ""W""o""r""l""d"
-;   std::cout 
+    constexpr thread_local static const char * const $ = const_cast<const char*>("H""e""l""l""o"" ""W""o""r""l""d")
+;   ::std::cout 
         << $[0]<<$[1]<<$[2]<<$[3]<<$[4]
         << static_cast<char>(26 + biscuit)
         << *&$+ biscuit
@@ -20,10 +25,10 @@ https://www.github.com/8-bit-pixel
         1+
         0)
     //??/
-    throw "nothing"
-;   exit(biscuit - 6)
+    goto https
+;   ::exit(biscuit - 6)
 ;}
 
 int main (int argc, char** argv) {
-    curse(6);
+    ::curse<int, std::ostream>(6);
 }
